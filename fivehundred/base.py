57384a3b0a4315b1056ab8fd9a2dc8fb2db8b9e7
Tom Witten <tom.witten@biarri.com>
2017-08-05 00:06:36 +1000

    add Enums for suits and values, add gen_game_deck funtion

--- fivehundred/base.py
+++ fivehundred/base.py
@@ -2,6 +2,29 @@
 Classic to represent basic objects like cards.
 """
 from collections import namedtuple
+from enum import Enum
+
+
+class CardSuits(Enum):
+    Spades = 'Spades'
+    Clubs = 'Clubs'
+    Diamonds = 'Diamonds'
+    Hearts = 'Hearts'
+
+
+class CardValues(Enum):
+    Four = 'Four'
+    Five = 'Five'
+    Six = 'Six'
+    Seven = 'Seven'
+    Eight = 'Eight'
+    Nine = 'Nine'
+    Ten = 'Ten'
+    Jack = 'Jack'
+    Queen = 'Queen'
+    King = 'King'
+    Ace = 'Ace'
+    Joker = 'Joker'
 
 
 # lightweight object to hold information about a person
@@ -10,7 +33,28 @@ class Card(namedtuple("Card", ["value", "suit"])):
 
     def __str__(self):
         # Special case because the joker doesn't have a suit?
-        if self.value == "Joker":
-            return self.value
+        if self.value == CardValues.Joker:
+            return self.value.value
 
-        return "{} of {}".format(self.value, self.suit)  # f strings where are you?
+        return "{} of {}".format(self.value.value, self.suit.value)  # f strings where are you?
+
+
+def suit_is_red(suit):
+    if suit in {CardSuits.Diamonds, CardSuits.Hearts}:
+        return True
+    return False
+
+
+def gen_game_deck():
+    for value in CardValues:
+        # Special case for Joker
+        if value == CardValues.Joker:
+            yield Card(value, None)
+            continue
+
+        for suit in CardSuits:
+            # Only red suits contain 4's
+            if value == CardValues.Four:
+                if not suit_is_red(suit):
+                    continue
+            yield Card(value, suit)
